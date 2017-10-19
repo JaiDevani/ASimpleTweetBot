1072287f74536ce9906d5fd1be7950b387238ca7
TheTylerHuskins <32919625+TheTylerHuskins@users.noreply.github.com>
2017-10-18 23:28:54 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-consumer_key = 'IyS5mb1PexXzWCyR9w1jpREU3'
-consumer_secret = 'FGVAWyTml4Cglhid479uBVhSzNrPUtC5WR6aAYo3cZZB5OF1KV'
-access_token = '917948804715991040-HIgo4xpkF3Yzh192AbgXgotwJtzumoK'
-access_token_secret = '0NYZFuujBFC3zMSD4loxNlQCLVFIqJDmBBItHb7JidM6o'
+consumer_key = 'xxx'
+consumer_secret = 'xxx'
+access_token = 'xxx'
+access_token_secret = 'xxx'
